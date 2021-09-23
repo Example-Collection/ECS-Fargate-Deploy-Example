82a8782aa7446e655e6bcf78856d0268f8623ced
sang-w0o <robbyra@gmail.com>
2021-09-23 12:28:27 +0900

    feat: Initial commit

---
+++ src/main/java/com/example/demo/BasicMessageResponseDto.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BasicMessageResponseDto {
+    private String message;
+}
